10ee7326a0bb6af370bd38b2df9830a036862c29
Wayne Wong <wwong312@my.bcit.ca>
2024-03-28 17:10:01 -0700

    Updated workouts page

--- scripts/workoutFunctions.js
+++ scripts/workoutFunctions.js
@@ -128,6 +128,50 @@ function displayWorkoutCard() {
 displayWorkoutCard();        //calling the function
 
 
+function displayExerciseCard() {
+  firebase.auth().onAuthStateChanged(function (user) {
+
+    let cardTemplate = document.getElementById("exerciseCardTemplate"); // Retrieve the HTML element with the ID "hikeCardTemplate" and store it in the cardTemplate variable. 
+
+    // db.collection("users").doc(user.uid).collection("workouts").get()
+    db.collection("exerciseTemplates").get()
+      .then(allExercises => {
+        allExercises.forEach(doc => {
+          // var workoutName = doc.data().name;
+          // var workout = doc.data();
+          var title = doc.data().name;       // get value of the "name" key
+          // var details = doc.data().details;  // get value of the "details" key
+          // var workoutImage = doc.data().imageName;    //get unique ID to each hike to be used for fetching right image
+          // var hikeLength = doc.data().length; //gets the length field
+          var docID = doc.id;
+          
+          let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+
+
+          console.log(doc.data());  //.data() returns data object
+          // console.log(workout.name);
+
+          //update title and text and image
+          newcard.querySelector('.exercise-title').innerHTML = title;
+          // newcard.querySelector('.card-length').innerHTML = hikeLength +"km";
+          // newcard.querySelector('.card-text').innerHTML = details;
+          // newcard.querySelector('.workout-image').src = `./images/${workoutImage}.jpg`; //Example: NV01.jpg
+          newcard.querySelector('a').href = "workingOut.html?docID="+docID;
+          
+
+          // document.getElementById("quote-goes-here").innerHTML = dayDoc.data().quote;  //using javascript to display the data on the right place
+          // document.getElementById("workoutName-goes-here").innerHTML = workout.name;  //using javascript to display the data on the right place
+          
+          //attach to gallery, Example: "hikes-go-here"
+          document.getElementById("exercises-go-here").appendChild(newcard);        });
+      });
+
+  });
+}
+
+displayExerciseCard();        //calling the function
+
+
 // Write New Workout Name to db
 function createWorkout() {
   firebase.auth().onAuthStateChanged(function (user) {
@@ -141,6 +185,9 @@ function createWorkout() {
     // .doc("WEYxpZPoNCvym1ig4cXL")
 
     .add({
+      dateAdded: firebase.firestore.FieldValue.serverTimestamp(),
+      favorite: false,
+      imageName: "elmo",
       name: document.getElementById("formNewWorkoutName").value
     })
 
@@ -155,6 +202,7 @@ function createWorkout() {
 
     .then(function (docRef) {
       console.log("Document written with ID: ", docRef.id);
+      window.location.assign("workout_favorite.html");       //re-direct to workout_favorite after creating a new workout
     })
 
     .catch(function (error) {
